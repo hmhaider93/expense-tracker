43e4bdf4eecbdecf778b87ff07ec48004ed52fac
Haider Mushtaq <hmhaider93@gmail.com>
2022-03-21 17:45:06 +0100

    creating basic expense component and subcomponents

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+import ExpenseDate from  "./ExpenseDate"
+import Card from "./Card";
+
+function ExpenseItem(props) {
+
+
+    const expenseTitle = props.title;
+    const expenseAmount = props.amount;
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date = {props.date}></ExpenseDate>
+			<div className="expense-item__description">
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price"> ${expenseAmount}</div>
+			</div>
+		</Card>
+	);
+}
+
+export default ExpenseItem;
